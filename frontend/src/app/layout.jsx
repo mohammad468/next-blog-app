b24ca0468b94b83fc46b8330049440828038004c
mohammad468 <mohammad.mohseni468@gmail.com>
2025-11-24 18:56:45 +0330

    add dark mode

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fa" dir="rtl">
+    <html lang="fa" dir="rtl" className="dark-mode">
       <body className={`${vazirFont.variable} font-sans min-h-screen`}>
         <Toaster />
         <AuthProvider>{children}</AuthProvider>
